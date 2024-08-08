fc52b3ae8395f5ba0520586f83c8e7a34ff73c36
Styx Dickert <styx@styxd.dev>
2024-08-08 20:14:13 +0000

    reorganize build.zig

--- build.zig
+++ build.zig
@@ -15,43 +15,33 @@ pub fn build(b: *std.Build) void {
     lib.root_module.addImport("websocket", websocket);
     b.installArtifact(lib);
 
-    const exe = b.addExecutable(.{
-        .name = "ping",
-        .root_source_file = b.path("examples/ping.zig"),
-        .target = target,
+    var dysnomia = b.addModule("dysnomia", .{
+        .root_source_file = b.path("src/dysnomia.zig"),
         .optimize = optimize,
+        .target = target,
     });
-    exe.root_module.addImport("dysnomia", &lib.root_module);
-    b.installArtifact(exe);
+    dysnomia.addImport("websocket", websocket);
 
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    buildExamples(b, target, optimize);
 
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/dysnomia.zig"),
         .target = target,
         .optimize = optimize,
     });
-
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+}
 
-    const exe_unit_tests = b.addTest(.{
+fn buildExamples(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) void {
+    const ping = b.addExecutable(.{
+        .name = "ping",
         .root_source_file = b.path("examples/ping.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    ping.root_module.addImport("dysnomia", b.modules.get("dysnomia").?);
+    const ping_step = b.step("ping", "build the ping example");
+    ping_step.dependOn(&ping.step);
 }
