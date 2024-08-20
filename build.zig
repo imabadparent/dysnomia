63b8b2f58839238210c95892a329e1b8b01e9d6d
Styx Dickert <styx@styxd.dev>
2024-08-20 15:57:49 -0400

    put minimum zig version in build.zig.zon

--- build.zig
+++ build.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
     const lib = b.addStaticLibrary(.{
